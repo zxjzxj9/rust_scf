0570482fb63d58f18d179276746d59e02f1ca9a0
Victor Zhang <zxjzxj9@gmail.com>
2024-12-07 02:27:38 +0800

    fix wrong code

--- src/basis/gto.rs
+++ src/basis/gto.rs
@@ -47,6 +47,73 @@ impl GTO1d {
         let n_squared = numerator * factor / denominator;
         n_squared.sqrt()
     }
+
+    fn evaluate(&self, x: f64) -> f64 {
+        self.norm * x.powi(self.l) * (-self.alpha * x.powi(2)).exp()
+    }
+
+    fn Sab(a: &GTO1d, b: &GTO1d) -> f64 {
+        let p = a.alpha + b.alpha;
+        let q = a.alpha * b.alpha / p;
+        let Q = a.c - b.c;
+        let P = (a.alpha * a.c + b.alpha * b.c) / p;
+        let Kab = (-q * Q.powi(2)).exp();
+
+        // Base case (i=0, j=0)
+        let mut S = vec![vec![0.0_f64; (b.l + 1) as usize]; (a.l + 1) as usize];
+        S[0][0] = Kab;
+
+        if a.l == 0 && b.l == 0 {
+            return S[0][0];
+        }
+
+        // Fill in S[i][0] for i>0
+        for i in 1..=a.l as usize {
+            let term1 = (P - a.c) * S[i-1][0];
+            let term2 = if i >= 2 {
+                (i as f64 - 1.0) / (2.0 * p) * S[i-2][0]
+            } else {
+                0.0
+            };
+            S[i][0] = term1 + term2;
+        }
+
+        // Fill in S[0][j] for j>0:
+        // S(0,j) = (P - B)*S(0,j-1) + (j-1)/(2p)*S(0,j-2)
+        for j in 1..=b.l as usize{
+            let term1 = (P - b.c) * S[0][j-1];
+            let term2 = if j >= 2 {
+                (j as f64 - 1.0) / (2.0 * p) * S[0][j-2]
+            } else {
+                0.0
+            };
+            S[0][j] = term1 + term2;
+        }
+
+        // Fill in the rest S[i][j] for i>0, j>0:
+        // S(i,j) = (P - A_x)*S(i-1,j) + (i-1)/(2p)*S(i-2,j)
+        //        + (P - B_x)*S(i,j-1) + (j-1)/(2p)*S(i,j-2)
+        for i in 1..=a.l as usize{
+            for j in 1..=b.l as usize {
+                let term1 = (P - a.c) * S[i-1][j];
+                let term2 = if i >= 2 {
+                    (i as f64 - 1.0) / (2.0 * p) * S[i-2][j]
+                } else {
+                    0.0
+                };
+                let term3 = (P - b.c) * S[i][j-1];
+                let term4 = if j >= 2 {
+                    (j as f64 - 1.0) / (2.0 * p) * S[i][j-2]
+                } else {
+                    0.0
+                };
+                S[i][j] = term1 + term2 + term3 + term4;
+            }
+        }
+
+        S[a.l as usize][b.l as usize] * (PI / p).sqrt() * a.norm * b.norm
+    }
+
 }
 
 // Simpson's rule integration
@@ -88,6 +155,17 @@ mod tests {
         // Check if integral is close to 1
         let diff = (integral - 1.0).abs();
         assert!(diff < 1e-5, "Integral is not close to 1: got {}", integral);
+    }
+
+    #[test]
+    fn test_gto_overlap() {
+        let gto1 = GTO1d::new(1.2, 3, 1.0);
+        let gto2 = GTO1d::new(1.5, 2, -1.0);
+        let integrand = |x: f64| gto1.evaluate(x) * gto2.evaluate(x);
+
+        let integral = simpson_integration(integrand, -10.0, 10.0, 10_000);
+        let overlap = GTO1d::Sab(&gto1, &gto2);
+        assert!((integral - overlap).abs() < 1e-5, "Overlap is not close to integral: got {}", overlap);
     }
 }
 
