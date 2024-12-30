98815ec4e0f957c57a8272c6a5fb5a43e9b3e699
Victor Zhang <zxjzxj9@gmail.com>
2024-12-30 11:55:11 +0800

    try to introduce load function

--- basis_library/src/lib.rs
+++ basis_library/src/lib.rs
@@ -1,15 +1,26 @@
+use basis::basis::Basis631G;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 // use basis;
 
-#[pyfunction]
-fn double(x: i32) -> i32 {
-    x * 2
-}
+// #[pyfunction]
+// fn load_basis_from_py_str(input: &str) -> Basis631G {
+//     Basis631G::parse_nwchem(input)
+// }
+
+// #[pyfunction]
+// fn dump_basis_to_pkl(basis: &Basis631G, filename: &str) -> std::io::Result<()> {
+//     basis.save_to_file(filename)
+// }
+//
+// #[pyfunction]
+// fn load_basis_from_pkl(filename: &str) -> Basis631G{
+//     Basis631G::load_from_file(filename).unwrap()
+// }
 
 #[pymodule]
 fn basis_py(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     // Add the `add` function to the Python module
-    m.add_function(wrap_pyfunction!(double, m)?)?;
+    // m.add_function(wrap_pyfunction!(load_basis_from_py_str, m)?)?;
     Ok(())
 }
