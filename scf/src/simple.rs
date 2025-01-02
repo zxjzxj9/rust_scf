c98b3957db4cc18b3a887cb2671af871ae338b96
Victor Zhang <zxjzxj9@gmail.com>
2025-01-03 01:40:53 +0800

    add some initial code

--- scf/src/simple.rs
+++ scf/src/simple.rs
@@ -8,25 +8,33 @@ use periodic_table_on_an_enum::Element;
 use crate::scf::SCF;
 use basis::basis::Basis;
 
-struct SimpleSCF {
+pub struct SimpleSCF<B: Basis> {
     num_atoms: u32,
+    mo_basis: Vec<B>,
     coords: Vec<Vector3<f64>>,
 }
 
-// impl SCF for SimpleSCF {
-//     // fn init_basis(&self, basis: HashMap<&str, &dyn Basis>) {
-//     //     println!("Initializing basis...");
-//     // }
-//     
-//     fn init_geometry(&self, coords: &Vec<Vector3<f64>>, elems: &Vec<Element>) {
-//         println!("Initializing geometry...");
-//     }
-// 
-//     fn init_density_matrix(&self) {
-//         println!("Initializing density matrix...");
-//     }
-// 
-//     fn init_fock_matrix(&self) {
-//         println!("Initializing Fock matrix...");
-//     }
-// }
+// implement the scf trait for the simple scf struct
+impl <B: Basis> SCF for SimpleSCF<B> {
+    type BasisType = B;
+
+    fn init_basis(&self, elems: &Vec<Element>, basis: HashMap<&str, &Self::BasisType>) {
+        println!("Initializing basis set...");
+    }
+
+    fn init_geometry(&self, coords: &Vec<Vector3<f64>>, elems: &Vec<Element>) {
+        println!("Initializing geometry...");
+    }
+
+    fn init_density_matrix(&self) {
+        println!("Initializing density matrix...");
+    }
+
+    fn init_fock_matrix(&self) {
+        println!("Initializing Fock matrix...");
+    }
+
+    fn scf_cycle(&self) {
+        println!("Performing SCF cycle...");
+    }
+}
