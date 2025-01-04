c23058d1dc70076539935471d17722aa1c4b4c2c
Victor Zhang <zxjzxj9@gmail.com>
2025-01-04 19:02:27 +0800

    add scf trait info

--- scf/src/scf.rs
+++ scf/src/scf.rs
@@ -8,9 +8,9 @@ use periodic_table_on_an_enum::Element;
 pub trait SCF {
     type BasisType: AOBasis;
     
-    fn init_basis(&self, elems: &Vec<Element>, basis: HashMap<&str, &Self::BasisType>);
-    fn init_geometry(&self, coords: &Vec<Vector3<f64>>, elems: &Vec<Element>);
-    fn init_density_matrix(&self);
-    fn init_fock_matrix(&self);
-    fn scf_cycle(&self);
+    fn init_basis(&mut self, elems: &Vec<Element>, basis: HashMap<&str, &Self::BasisType>);
+    fn init_geometry(&mut self, coords: &Vec<Vector3<f64>>, elems: &Vec<Element>);
+    fn init_density_matrix(&mut self);
+    fn init_fock_matrix(&mut self);
+    fn scf_cycle(&mut self);
 }
--- scf/src/simple.rs
+++ scf/src/simple.rs
@@ -18,23 +18,31 @@ pub struct SimpleSCF<B: AOBasis> {
 impl <B: AOBasis> SCF for SimpleSCF<B> {
     type BasisType = B;
 
-    fn init_basis(&self, elems: &Vec<Element>, basis: HashMap<&str, &Self::BasisType>) {
+    fn init_basis(&mut self, elems: &Vec<Element>, basis: HashMap<&str, &Self::BasisType>) {
+        self.num_atoms = elems.len() as u32;
         println!("Initializing basis set...");
+
+        for elem in elems {
+            println!("Element: {}", elem);
+        }
+
+        println!("Number of atoms: {}", self.num_atoms);
+        println!("Number of basis functions: {}", self.mo_basis.len());
     }
 
-    fn init_geometry(&self, coords: &Vec<Vector3<f64>>, elems: &Vec<Element>) {
+    fn init_geometry(&mut self, coords: &Vec<Vector3<f64>>, elems: &Vec<Element>) {
         println!("Initializing geometry...");
     }
 
-    fn init_density_matrix(&self) {
+    fn init_density_matrix(&mut self) {
         println!("Initializing density matrix...");
     }
 
-    fn init_fock_matrix(&self) {
+    fn init_fock_matrix(&mut self) {
         println!("Initializing Fock matrix...");
     }
 
-    fn scf_cycle(&self) {
+    fn scf_cycle(&mut self) {
         println!("Performing SCF cycle...");
     }
 }
