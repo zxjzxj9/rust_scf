77a4da7031290a096ce6836d9cc912a3c6c4110a
Victor Zhang <zxjzxj9@gmail.com>
2025-02-28 17:45:11 +0800

    add example code for diis

--- scf/src/scf.rs
+++ scf/src/scf.rs
@@ -3,6 +3,7 @@ use basis::basis::AOBasis;
 use na::Vector3;
 use periodic_table_on_an_enum::Element;
 use std::collections::HashMap;
+use nalgebra::{DMatrix, DVector};
 
 pub trait SCF {
     type BasisType: AOBasis;
@@ -13,4 +14,35 @@ pub trait SCF {
     fn update_density_matrix(&mut self);
     fn init_fock_matrix(&mut self);
     fn scf_cycle(&mut self);
+}
+
+struct DIIS {
+    error_vectors: Vec<DVector<f64>>,
+    fock_matrices: Vec<DMatrix<f64>>,
+    max_subspace_size: usize,
+}
+
+impl DIIS {
+    fn new(max_subspace_size: usize) -> Self {
+        DIIS {
+            error_vectors: Vec::new(),
+            fock_matrices: Vec::new(),
+            max_subspace_size,
+        }
+    }
+
+    fn update(&mut self, error_vector: DVector<f64>, fock_matrix: DMatrix<f64>) {
+        if self.error_vectors.len() >= self.max_subspace_size {
+            self.error_vectors.remove(0);
+            self.fock_matrices.remove(0);
+        }
+        self.error_vectors.push(error_vector);
+        self.fock_matrices.push(fock_matrix);
+    }
+
+    fn extrapolate(&self) -> DMatrix<f64> {
+        // Implement DIIS extrapolation logic here
+        // Return the extrapolated Fock matrix
+        return DMatrix::zeros(1, 1);
+    }
 }
