8aec08f39030c7c12921fa68d44985b5277e5492
Victor Zhang <zxjzxj9@gmail.com>
2025-09-07 21:02:26 +0800

    update config

--- md/src/lib.rs
+++ md/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod lj_pot;
 pub mod run_md;
+pub mod config;
 
 pub use lj_pot::LennardJones;
 pub use run_md::{ForceProvider, Integrator, NoseHooverVerlet, NoseHooverParrinelloRahman};
+pub use config::*;
