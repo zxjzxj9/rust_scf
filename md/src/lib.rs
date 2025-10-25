8f011c907f49151e01b9df0c30f84e9ee59bd9d0
Victor Zhang <zxjzxj9@gmail.com>
2025-10-25 22:03:24 +0800

    update lib for gcmc

--- md/src/lib.rs
+++ md/src/lib.rs
@@ -2,7 +2,9 @@ pub mod lj_pot;
 pub mod run_md;
 pub mod config;
 pub mod ising;
+pub mod gcmc;
 
 pub use lj_pot::LennardJones;
 pub use run_md::{ForceProvider, Integrator, NoseHooverVerlet, NoseHooverParrinelloRahman};
 pub use config::*;
+pub use gcmc::{GCMC, GCMCStatistics, GCMCResults, parallel_gcmc_sweep};
